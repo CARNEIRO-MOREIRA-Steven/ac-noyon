1ba49911e7f0c86de59a93878fdcf411746ad42d
Squaad6z <carneiromoreirasteven22@gmail.com>
2024-10-07 14:47:06 +0200

    Ajout calendrier Hompage + Responsive

--- src/app/components/FuturEvents.jsx
+++ src/app/components/FuturEvents.jsx
@@ -1,10 +1,15 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 import './futur-evenement.css';
 
 const FuturEvents = () => {
   const [futurEvenements, setFuturEvenements] = useState([]);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [value, setValue] = useState(new Date());
+  const [showCalendar, setShowCalendar] = useState(false);
 
   const fetchEvenements = async () => {
     try {
@@ -15,10 +20,20 @@ const FuturEvents = () => {
       const data = await res.json();
       console.log("Données reçues :", data);
 
-      // Trier les événements par date
-      const sortedEvenements = data.data.sort((a, b) => new Date(a.Date) - new Date(b.Date));
+      const sortedEvenements = data.data.map(evenement => {
+        const originalDate = new Date(evenement.date);
+        
+        const calendarDate = new Date(originalDate);
+        calendarDate.setDate(calendarDate.getDate() - 1);
+        
+        return {
+          ...evenement,
+          date: originalDate.toISOString().split('T')[0],
+          calendarDate: calendarDate.toISOString().split('T')[0], 
+          time: originalDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }) 
+        };
+      }).sort((a, b) => new Date(a.calendarDate) - new Date(b.calendarDate)); 
 
-      // Prendre seulement les 6 derniers événements
       setFuturEvenements(sortedEvenements.slice(0, 6) || []);
     } catch (error) {
       console.error("Erreur lors de la récupération des événements :", error);
@@ -27,39 +42,51 @@ const FuturEvents = () => {
 
   useEffect(() => {
     fetchEvenements();
+    setShowCalendar(true);
   }, []);
+
+  const handleDateSelect = (date) => {
+    const selectedDate = date.toISOString().split('T')[0];
+    const event = futurEvenements.find((evenement) => {
+      return evenement.calendarDate === selectedDate;
+    });
+    
+    if (event) {
+      setSelectedEvent(event); 
+    } else {
+      setSelectedEvent(null); 
+    }
+  };
 
   return (
     <section className='futur_events_section'>
       <section className='futur_events_container'>
         <section className='evenement_a_venir'>
-          <h2 className='evenement_a_venir_title'>Événements à venir</h2>
-          <ul>
-            {futurEvenements.length > 0 ? (
-              futurEvenements.map((evenement) => (
-                <li key={evenement.id}>
-                  <p>{evenement.name}</p>
-                </li>
-              ))
-            ) : (
-              <p>Aucun événement à afficher.</p>
-            )}
-          </ul>
+          {showCalendar && (
+            <Calendar
+              onChange={handleDateSelect}
+              value={value}
+              tileClassName={({ date }) => {
+                const formattedDate = date.toISOString().split('T')[0];
+                const isEventDate = futurEvenements.some(evenement => {
+                  return evenement.calendarDate === formattedDate;
+                });
+                return isEventDate ? 'marked-date' : null; 
+              }}
+            />
+          )}
         </section>
-
         <section className='date_evenement_a_venir'>
-          <h2 className='evenement_a_venir_title'>Date des événements</h2>
-          <ul>
-            {futurEvenements.length > 0 ? (
-              futurEvenements.map((evenement) => (
-                <li key={evenement.id}>
-                  <p>{new Date(evenement.date).toLocaleString()}</p>
-                </li>
-              ))
-            ) : (
-              <p>Aucun événement à afficher.</p>
-            )}
-          </ul>
+          {selectedEvent ? (
+            <div className="event-details">
+              <h2>Détails de l'événement</h2>
+              <h3>Nom : {selectedEvent.name}</h3>
+              <h3>Date : {new Date(selectedEvent.date).toLocaleDateString()}</h3>
+              <h3>Heure : {selectedEvent.time}</h3>
+            </div>
+          ) : (
+            <h3>Aucun événement prévu à cette date.</h3>
+          )}
         </section>
       </section>
     </section>
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import React, {useEffect} from 'react'
 import Header from './components/Header'
 import Banner from './components/Banner'
 import FuturEvents from './components/FuturEvents'
@@ -6,6 +7,23 @@ import Inscription_intro from './components/Inscription_intro'
 import Footer from './components/Footer'
 
 const page = () => {
+  useEffect(() => {
+    const pingApi = () => {
+      fetch('https://ac-noyon-strapi-app.onrender.com/')
+        .then(response => {
+          if (response.ok) {
+            console.log('API est active');
+          } else {
+            console.error('Erreur lors de l’accès à l’API');
+          }
+        })
+        .catch(error => console.error('Erreur:', error));
+    };
+
+    const intervalId = setInterval(pingApi, 800000);
+
+    return () => clearInterval(intervalId);
+  }, []);
   return (
     <section>
       <Header />
